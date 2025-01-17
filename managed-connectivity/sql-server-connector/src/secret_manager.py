90f8e5083571564c9ea3c7b3679da0d9d07febfd
danielholgate <dholgate@gmail.com>
2025-01-17 17:32:20 +0100

    SQL Server secretsmanager

--- managed-connectivity/sql-server-connector/src/secret_manager.py
+++ managed-connectivity/sql-server-connector/src/secret_manager.py
@@ -6,3 +6,7 @@ def get_password(secret_path: str) -> str:
     """Gets password from a GCP service."""
     client = secretmanager.SecretManagerServiceClient()
     if "versions" not in secret_path:
+        # If not specified, we need the latest version of a password
+        secret_path = f"{secret_path}/versions/latest"
+    response = client.access_secret_version(request={"name": secret_path})
+    return response.payload.data.decode("UTF-8")
