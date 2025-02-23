93018d331852b10ee4d5a8bde6230dc3a40591f3
danielholgate <dholgate@gmail.com>
2025-02-23 13:47:53 +0100

    readme and command line

--- managed-connectivity/mysql-connector/src/cmd_reader.py
+++ managed-connectivity/mysql-connector/src/cmd_reader.py
@@ -9,9 +9,9 @@ def read_args():
 
     # Project arguments
     parser.add_argument("--target_project_id", type=str, required=True,
-                        help="The name of the target Google Cloud project to import the metadata into.")
+                        help="Name of the target Google Cloud project to import the metadata into. Generated Entries will use this project")
     parser.add_argument("--target_location_id", type=str, required=True,
-                        help="The target Google Cloud location where the metadata will be imported into.")
+                        help="Target Google Cloud location metadata will be imported into. Generated Entries will use this location")
     parser.add_argument("--target_entry_group_id", type=str, required=True,
                         help="The ID of the Dataplex Entry Group to import metadata into. "
                              "Metadata will be imported into entry group with the following"
@@ -23,18 +23,16 @@ def read_args():
                         help="Mysql host server")
     parser.add_argument("--port", type=str, required=True,
                         help="The port number (usually 3306)")
+    parser.add_argument("--database", type=str, required=True,
+                        help="Mysql database to connect to")
     parser.add_argument("--user", type=str, required=True, help="Mysql User")
     parser.add_argument("--password-secret", type=str, required=True,
                         help="Resource name in the Google Cloud Secret Manager for the Mysql password")
-    parser.add_argument("--database", type=str, required=True,
-                        help="Mysql database to connect to")
-    #parser.add_argument("--exclude-schemas", type=str,required=False,
-    #    help="Additional schemas to be excluded from metadata extract (comma seperated list)")
 
     # Google Cloud Storage arguments
     # It is assumed that the bucket is in the same region as the entry group
     parser.add_argument("--output_bucket", type=str, required=True,
-                        help="The Cloud Storage bucket to write the generated metadata import file. Format begins with gs:// ")
+                        help="The Cloud Storage bucket to write the generated metadata import file. Do not include gs:// ")
     parser.add_argument("--output_folder", type=str, required=True,
                         help="The folder within the Cloud Storage bucket, to write the generated metadata import files. Name only required")
 
